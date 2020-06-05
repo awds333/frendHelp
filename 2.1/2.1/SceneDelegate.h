b10a532ad9f437a578463a03c9578530cb4936df
yura f <wn8yura@gmail.com>
2020-06-06 02:25:11 +0300

    a

---
+++ 2.1/2.1/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  lab8_task1
+//
+//  Created by Pavel on 4/21/20.
+//  Copyright © 2020 pavel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ 2.1/2.1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lab8_task1
+//
+//  Created by Pavel on 4/21/20.
+//  Copyright © 2020 pavel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
