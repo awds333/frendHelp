b2399c79c30ef085e1fc7f6d2e91f0656d77aaba
Fedchenko Yura <wn8yura@gmail.com>
2020-06-05 21:13:08 -0700

    f

---
+++ lab9Final_v2/Task1.2/lab9Gulyaev1.2/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lab9Gulyaev1.2
+//
+//  Created by Vlad Gulyaev on 05.06.2020.
+//  Copyright © 2020 Vlad Gulyaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
+
